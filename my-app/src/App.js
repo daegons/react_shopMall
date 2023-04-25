5ad04163995742ef9192c597a281dd0bcb78ab0b
daegons <119998807+daegons@users.noreply.github.com>
2023-04-25 18:34:36 +0900

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,32 @@
+import Container from "react-bootstrap/Container";
+import "./App.css";
+
+//data.js import
+import data from "./data";
+
+//Navbar bootstrap import
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useState } from "react";
+//components
+import ProductList from "./components/ProductList";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // bootstrap 사이트에서 이import로 안해주면 오류생김
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import './App.css';
+//인라인 img import하는 법
+// import 이름작성 from "./img/BredList.jpg";
 
 function App() {
+  // const road = process.env.PUBLIC_URL;
+
+  const [shoes] = useState(data);
+
   return (
     <div className="App">
-      <Navbar bg="light" variant="light">
+      <Navbar bg="light" variant="light" className="navbar">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          {/* 인라인으로 img 넣는 법 */}
+          {/* <div style={{ backgroundImage : `url('${작명}')`}}></div> */}
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -18,7 +34,14 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
       <div className="main_img"></div>
+
+      <div className="container">
+        <div className="row">
+          <ProductList shoes={shoes} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ my-app/src/data.js
@@ -0,0 +1,47 @@
+const data = [
+  {
+    id: 1,
+    title: "Jordan 1 Retro Bred",
+    content: "조던 레트로 브레드 1994",
+    price: 2800000,
+  },
+
+  {
+    id: 2,
+    title: "Adidas Yeezy Boost Beluga",
+    content: "이지 부스트 350 V2 벨루가 2.0",
+    price: 650000,
+  },
+
+  {
+    id: 3,
+    title: "Jordan 11 Retro Concord",
+    content: "조던 레트로 콩코드 2018",
+    price: 470000,
+  },
+  {
+    id: 4,
+    title: "Nike Air Force 1 Mid",
+    content: "나이키 에어포스 1 미드 '07 펄 화이트",
+    price: 189000,
+  },
+  {
+    id: 5,
+    title: "Jordan 1 x Off-White Retro High",
+    content: "조던 1 x 오프화이트 레트로 하이 화이트",
+    price: 6800000,
+  },
+];
+
+export default data;
+
+// export default {여러,개,할수있음};
+// import {여러,개,임폴트} 여러개는 작명 불가!!
+
+//오브젝트 자료형에서
+
+// let b = {
+//   key: value,
+// };
+
+// let a = ["daegon", 20]; 이런식으로..
